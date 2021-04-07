3746d12d2be73112cb42ab6b63d9631a195b1983
Danil1Zamariy <75875975+Danil1Zamariy@users.noreply.github.com>
2021-04-07 19:37:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,8 @@ int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
+      if (arr[i] + arr[j] == value)
         count++;
-      }
     }
   }
   return count;
@@ -16,21 +15,18 @@ int countPairs2(int *arr, int len, int value) {
   int count = 0, left = 0, right = len - 1;
     while (left < right - 1) {
         int middle = (left + right) / 2;
-        if (arr[middle] <= value){
+        if (arr[middle] <= value)
             left = middle;
-        } else{
+        else
             right = middle;
-        }
         }
     len = right-1;
     for (int i = len; i >=0; i--) {
         for (int j = 0; j < i; j++) {
-            if (arr[i] + arr[j] == value){
+            if (arr[i] + arr[j] == value)
                 count++;
-            }
-            if (arr[i] + arr[j] > value){
+            if (arr[i] + arr[j] > value)
                 break;
-            }
         }
     }
     return count;
@@ -40,11 +36,10 @@ int countPairs3(int *arr, int len, int value) {
   int count = 0, left = 0, right = len - 1;
     while (left < right - 1) {
         int middle = (left + right) / 2;
-        if (arr[middle] <= value){
+        if (arr[middle] <= value)
             left = middle;
-        } else{
+        else
             right = middle;
-        }
     }
     len = right - 1;
   for (int i = 0; i <len; i++) {
@@ -52,11 +47,10 @@ int countPairs3(int *arr, int len, int value) {
         int num = 0;
         while (left < right) {
             int m = (left + right) / 2;
-            if (arr[m] < (value-arr[i])){
+            if (arr[m] < (value-arr[i]))
                 left = m + 1;
-            } else{
+            else
                 right = m;
-            }
         }
     while (arr[left] == (value - arr[i])) {
         num++;
